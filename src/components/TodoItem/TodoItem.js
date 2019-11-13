8b9d5ef7d45357fa97c992bb24009a46cdf40310
Serhii Utiupin <sergey.utupin@gmail.com>
2019-11-13 21:20:23 +0200

    added edit, save and cancel buttons

--- src/components/TodoItem/TodoItem.js
+++ src/components/TodoItem/TodoItem.js
@@ -1,8 +1,12 @@
-import React from 'react';
+import React, { useState, useRef } from 'react';
 
 export default function TodoItem(props) {
 
-    const handleChange = async () => {
+    const [editable, setEditable] = useState(false);
+    const inputEl = useRef(null);
+
+    const handleIsDone = async () => {
+        setEditable(false);
         const { item, handleUpdate } = props;
         const itemCopy = {...item};
         itemCopy.isDone = !item.isDone;
@@ -15,51 +19,68 @@ export default function TodoItem(props) {
     };
 
     const handleEditItem = () => {
-
+        setEditable(true);
     };
 
     const handleSaveItem = () => {
-
+        const { item, handleUpdate } = props;
+        const itemCopy = {...item};
+        itemCopy.summary = inputEl.current.value;
+        handleUpdate(itemCopy);
+        setEditable(false);
     };
+
+    const handleCancelEdit = () => {
+        setEditable(false);
+    }
 
     const { summary, isDone } = props.item;    
     const style = isDone ? {textDecoration: "line-through"} : {};
 
+    let editableText;
+    if(editable) {
+        editableText = (
+            <>
+                <input type='checkbox' checked={ isDone } onChange={ handleIsDone }/> 
+                <input ref={ inputEl } type="text" defaultValue={ summary } /> 
+            </>
+        );
+    } else {
+        editableText = (
+            <label>
+                <input type='checkbox' checked={ isDone } onChange={ handleIsDone }/> 
+                { summary }
+            </label> 
+        );
+    }
+ 
     return (
         <li>
             <form style={style}>
-                <input type='checkbox' checked={ isDone } onChange={ handleChange }/>
-                <EditableText text={ summary } />
-                <EditOrSaveButton handleEdit={ handleEditItem } handleSave={handleSaveItem} mode='save'/>
+                {editableText}
+                <Buttons editable={ editable }
+                    handleEdit={ handleEditItem } 
+                    handleSave={ handleSaveItem } 
+                    handleCancel ={ handleCancelEdit } 
+                />
                 <input type='button' value='delete' onClick={ handleDeleteItem } />
             </form>
         </li>
     ); 
 }
 
-function EditableText(props) {
-    const { text, editable} = props;
+function Buttons(props) {
+    const { editable, handleEdit, handleSave, handleCancel } = props;
     if(editable) {
         return (
-            <input type="text" value={text} />    
+            <>
+                <input type='button' value='save' onClick={ handleSave } />
+                <input type='button' value='cancel' onClick={ handleCancel } />
+            </>
         )
-    } else {
-        return (
-            <>{text}</>    
-        )
-    }
-}
-
-function EditOrSaveButton(props) {
-    const {mode = 'edit', handleEdit, handleSave } = props;
-    if(mode === 'edit') {
+    } else  {
         return (
             <input type='button' value='edit' onClick={ handleEdit } />
         )
     }
-    if(mode === 'save') {
-        return (
-            <input type='button' value='save' onClick={ handleSave } />
-        )
-    }    
 }
