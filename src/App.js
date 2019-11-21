b0090440a3630570ed943a1a7df3a31845d585db
sutiupin <sutiupin@rocketsoftware.com>
2019-11-21 17:34:14 +0200

    material-ui: stage 2

--- src/App.js
+++ src/App.js
@@ -20,51 +20,58 @@ import { Provider } from 'react-redux'
 import { createStore } from 'redux'
 import combinedReducer from './engine/reducers'
 
-import ButtonAppBar from './components/ButtonAppBar/ButtonAppBar';
 import SimpleBottomNavigation from './components/SimpleBottomNavigation/SimpleBottomNavigation'
+
+import AppBar from '@material-ui/core/AppBar';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+
 
 const store = createStore(
   combinedReducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()  
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
+
+const LinkAll = React.forwardRef((props, ref) => (
+  <Link innerRef={ref} to="/" {...props} />
+));
+
+const LinkDone = React.forwardRef((props, ref) => (
+  <Link innerRef={ref} to="/done" {...props} />
+));
+
+const LinkNotDone = React.forwardRef((props, ref) => (
+  <Link innerRef={ref} to="/notdone" {...props} />
+));
+
 
 export default function App() {
   return (
     <Provider store={store}>
-
-    <ButtonAppBar />
-
-
-    <Router>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/" >All todos</Link>
-            </li>
-             <li>
-            <Link to="/done" >Done todos</Link>
-            </li>
-            <li>
-              <Link to="/notdone" >Not done todos</Link>
-            </li>
-           </ul>
-        </nav>
+      <Router>
+        <AppBar position="static">
+          <Tabs>
+            <Tab label="All todos" component={LinkAll} />
+            <Tab label="Done todos" component={LinkDone} />
+            <Tab label="Not done todos" component={LinkNotDone} />
+          </Tabs>
+        </AppBar>
 
         <Switch>
-           <Route path="/notdone">
-            <TodoList key={ "notdone" } filter={{ field: "isDone", value: false }} />
-          </Route>          
+          <Route path="/notdone">
+            <TodoList key={"notdone"} filter={{ field: "isDone", value: false }} />
+          </Route>
           <Route path="/done">
-            <TodoList key={ "done" } filter={{ field: "isDone", value: true }} />
+            <TodoList key={"done"} filter={{ field: "isDone", value: true }} />
           </Route>
           <Route path="/">
-            <TodoList key={ "all" } />
+            <TodoList key={"all"} />
           </Route>
         </Switch>
-      </div>
-    </Router>
-    <SimpleBottomNavigation />
+      </Router>
+
+
+      <SimpleBottomNavigation />
     </Provider>
   );
 }
